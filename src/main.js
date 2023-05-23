4b8c55d92084ade8a302e8ad9a29447197dd1a04
Leonardo Nunes <leonardonunesfava@gmail.com>
2023-05-23 18:08:47 -0300

    pause

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-// import { nanoid } from 'nanoid'
-// import "./style.css";
-fetch('https://api.exchangerate.host/latest?base=${moeda}')
-  .then();
+import './style.css';
+
+const submit = document.getElementById('search');
+const receive = document.getElementById('moeda');
+const recebeMoeda = (moeda) => {
+  fetch(`https://api.exchangerate.host/latest?base=${moeda}`)
+    .then((response) => console.log(response));
+};
+submit.addEventListener('click', () => {
+  recebeMoeda(receive.value);
+});
